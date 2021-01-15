c7eb5b60b0d2d8d015e7158644ebe06d370bad46
ogis-mori <mori_miki@ogis-ri.co.jp>
2021-01-15 15:52:49 +0900

    step1

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,36 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="ToDoを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTodo</p>
+        <ul>
+          <div>
+            <li>あああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>いいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTodo</p>
+        <ul>
+          <div>
+            <li>ううう</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
